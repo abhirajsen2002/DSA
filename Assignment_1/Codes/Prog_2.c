dc156e1f84e52c1d846424655fde80ae8ec1ee8f
Abhiraj Sen <80464878+abhirajsen2002@users.noreply.github.com>
2022-02-04 23:35:02 +0530

    Update Prog_2.c

--- Assignment_1/Codes/Prog_2.c
+++ Assignment_1/Codes/Prog_2.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
-double fibo(double n)
+long long int fibo(long long int n)
 {
-	if(n==1)
+	if (n == 1)
 		return 0;
-	else if(n==2)
+	else if (n == 2)
 		return 1;
-	else 
-		return fibo(n-1)+fibo(n-2);//the recursive call statement
+	else
+		return fibo(n - 1) + fibo(n - 2); // the recursive call statement
 }
-double fibo_iterative(double n)
+long long int fibo_iterative(long long int n)
 {
-	if(n==1)
+	if (n == 1)
 		return 0;
-	else if(n==2)
+	else if (n == 2)
 		return 1;
-	double a=0;
-	double b=1;
-	n-=2;
-	while(n-->0)//iterative way to find the nth fibo no
+	long long int a = 0;
+	long long int b = 1;
+	n -= 2;
+	while (n-- > 0) // iterative way to find the nth fibo no
 	{
-		double c=a+b;
-		a=b;
-		b=c;
+		long long int c = a + b;
+		a = b;
+		b = c;
 	}
 	return b;
 }
 int main()
 {
-	double n=31;
-	n=fibo(n);
-	printf("%lf",n);
+	long long int n = 93;
+	long long int temp = 0;
+	temp = fibo(n);
+	printf("N = %d", n);
+	printf("\nUsing recursive method %dth fibonacci number = %d", n, temp);
+	temp = fibo_iterative(n);
+	printf("\nUsing iterative method %dth fibonacci number = %d", n, temp);
+
+	n = 94;
+	temp = fibo(n);
+	printf("\n\nN = %d", n);
+	printf("\nUsing recursive method %dth fibonacci number = %d", n, temp);
+	temp = fibo_iterative(n);
+	printf("\nUsing iterative method %dth fibonacci number = %d", n, temp);
 }
